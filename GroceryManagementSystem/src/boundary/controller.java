833c17456fd084ef2af5aba91b8968b6d3112ae3
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-06-25 11:44:03 +0700

    add boudary

---
+++ GroceryManagementSystem/src/boundary/controller.java
@@ -0,0 +1,12 @@
+package boundary;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class controller {
+
+    @FXML
+    private TableColumn<?, ?> tb_expiring;
+
+}
+
--- GroceryManagementSystem/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author Helscarthe
- *
- */
-module GroceryManagementSystem {
-}
