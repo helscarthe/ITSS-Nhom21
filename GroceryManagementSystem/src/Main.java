8517ac21dc82cfe2eb960b894a45489c9f7d721c
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-06-28 15:41:50 +0700

    add main test fxml

---
+++ GroceryManagementSystem/src/Main.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage){
+		// TODO Auto-generated method stub
+		try {
+	    	Parent login = FXMLLoader.load(Main.class.getResource("/boundary/Login.fxml"));
+			Scene scene = new Scene(login);
+	//		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace(System.out);
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println("entry");
+		launch(args);
+	}
+
+}
