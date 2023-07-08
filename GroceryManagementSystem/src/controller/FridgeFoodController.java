7ec9499ee5d36e1b4a81ab17645b153d6aa1fcba
Administrator <Administrator@S-14>
2023-07-09 04:13:59 +0700

    add fridge

--- GroceryManagementSystem/src/controller/FridgeFoodController.java
+++ GroceryManagementSystem/src/controller/FridgeFoodController.java
@@ -1,10 +1,210 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+
+import entity.FridgeEntity;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import service.SqliteConnection;
+
 public class FridgeFoodController {
 
-	public FridgeFoodController() {
-		// TODO Auto-generated constructor stub
+	public boolean deleteFood(int foodId, int userId) {
+
+		try (Connection conn = SqliteConnection.Connector(); Statement sttm = conn.createStatement();) {
+
+			sttm.executeUpdate("delete from fridge_food where user_id=" + String.valueOf(userId)
+					+ " and food_id=" + String.valueOf(foodId) + ";");
+
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
 	}
 	
+	public ObservableList<FridgeEntity> getFoodListByUserId(int userId) {
+
+		ObservableList<FridgeEntity> foodList = FXCollections.observableArrayList();
+
+		String foodListQuery = "select fp.fridge_food_id, fp.food_id, fp.number, fp.expiry_date, rf.raw_food_name, rf.food_type, rf.unit from fridge_food as fp, raw_foods as rf where fp.food_id = rf.raw_food_id and fp.user_id="
+				+ String.valueOf(userId) + ";";
+
+		try (Connection conn = SqliteConnection.Connector(); Statement sttm = conn.createStatement();) {
+
+			ResultSet rs = sttm.executeQuery(foodListQuery);
+
+			while (rs.next()) {
+				if (!rs.getString("expiry_date").equals(LocalDate.now().toString())) {
+					foodList.add(new FridgeEntity(rs.getInt("fridge_food_id"), rs.getString("raw_food_name"),
+							rs.getInt("food_id"), rs.getInt("number"), rs.getString("expiry_date"), rs.getInt("food_type"),
+							rs.getString("unit")));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return foodList;
+	}
+
+	public ObservableList<FridgeEntity> getFoodListByType(String typeFood) {
+
+		ObservableList<FridgeEntity> foodList = FXCollections.observableArrayList();
+
+		int foodTypeInt;
+
+		if (typeFood.equalsIgnoreCase("Rau")) {
+			foodTypeInt = 1;
+		} else if (typeFood.equalsIgnoreCase("Thịt")) {
+			foodTypeInt = 2;
+		} else {
+			foodTypeInt = 3;
+		}
+
+		String query = "select fp.fridge_food_id, fp.food_id, fp.number, fp.expiry_date, rf.raw_food_name, rf.food_type, rf.unit from fridge_food as fp, raw_food as rf where fp.food_id = rf.raw_food_id and fp.food_type="
+				+ foodTypeInt + ";";
+
+		try (Connection conn = SqliteConnection.Connector(); Statement sttm = conn.createStatement();) {
+
+			ResultSet rs = sttm.executeQuery(query);
+
+			while (rs.next()) {
+				foodList.add(new FridgeEntity(rs.getInt("fridge_food_id"), rs.getString("raw_food_name"),
+						rs.getInt("food_id"), rs.getInt("number"), rs.getString("expiry_date"), rs.getInt("food_type"),
+						rs.getString("unit")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return foodList;
+
+	}
+
+	public FridgeEntity getFoodByTypeAndName(String typeFood, String nameFood) {
+
+		int foodTypeInt;
+
+		if (typeFood.equalsIgnoreCase("Rau")) {
+			foodTypeInt = 1;
+		} else if (typeFood.equalsIgnoreCase("Thịt")) {
+			foodTypeInt = 2;
+		} else {
+			foodTypeInt = 3;
+		}
+
+		String query = "select fp.fridge_food_id, fp.food_id, fp.number, fp.expiry_date, rf.raw_food_name, rf.food_type, rf.unit from fridge_food as fp, raw_food as rf where fp.food_id = rf.raw_food_id and fp.food_type="
+				+ foodTypeInt + "and rf.raw_food_name='" + nameFood + "';";
+
+		try (Connection conn = SqliteConnection.Connector(); Statement sttm = conn.createStatement();) {
+
+			ResultSet rs = sttm.executeQuery(query);
+
+			if (rs.next()) {
+				return new FridgeEntity(rs.getInt("fridge_food_id"), rs.getString("raw_food_name"),
+						rs.getInt("food_id"), rs.getInt("number"), rs.getString("expiry_date"), rs.getInt("food_type"),
+						rs.getString("unit"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return null;
+
+	}
+	
+	public FridgeEntity getFoodModelByName(String nameFood) {
+
+
+		String query = "select * from raw_foods where raw_food_name= '" + nameFood + "' collate nocase;";
+
+		try (Connection conn = SqliteConnection.Connector(); Statement sttm = conn.createStatement();) {
+
+			ResultSet rs = sttm.executeQuery(query);
+
+			if (rs.next()) {
+				return new FridgeEntity(rs.getInt("raw_food_id"), rs.getString("raw_food_name"), rs.getInt("food_type"),
+						rs.getString("unit"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return null;
+
+	}
+
+	public int getFoodIdByName(String nameFood) {
+
+		String query = "select raw_food_id from raw_foods where raw_food_name= '" + nameFood + "' collate nocase;";
+
+		try (Connection conn = SqliteConnection.Connector(); Statement sttm = conn.createStatement();) {
+
+			ResultSet rs = sttm.executeQuery(query);
+
+			if (rs.next()) {
+				return rs.getInt("raw_food_id");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+
+		return 0;
+	}
+	
+	public boolean insertFoodIntoFridge(String nameFood, int userId, int number, String expiry_date) {
+
+		int foodId = getFoodIdByName(nameFood);
+		
+		if (foodId != 0) {
+			String insertIntoFridge = "insert into fridge_food (user_id, food_id, number, expiry_date) values (?, ?, ?, ?);";
+
+			try (Connection conn = SqliteConnection.Connector();
+					PreparedStatement psttm = conn.prepareStatement(insertIntoFridge);) {
+				psttm.setInt(1, userId);
+				psttm.setInt(2, foodId);
+				psttm.setInt(3, number);
+				psttm.setString(4, expiry_date);
+				
+
+				if (psttm.execute()) {
+					return true;
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+		} 
+		
+		return false;
+
+	}
+	
+	public void updateFood(int foodId, int userId, int soLuong) {
+
+		String query = "update fridge_food set number = ? where food_id = ? and user_id = ?;";
+
+		try (Connection conn = SqliteConnection.Connector(); PreparedStatement psttm = conn.prepareStatement(query);) {
+
+			psttm.setInt(1, soLuong);
+			psttm.setInt(2, foodId);
+			psttm.setInt(3, userId);
+
+			psttm.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
---
+++ GroceryManagementSystem/src/view/fridge/FridgeHandler.java
@@ -0,0 +1,179 @@
+package view.fridge;
+
+import java.time.LocalDate;
+
+import controller.FridgeFoodController;
+import entity.FridgeEntity;
+import entity.UserEntity;
+import entity.UserSingleton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+public class FridgeHandler {
+	 @FXML
+	    private Button btnChon;
+
+	    @FXML
+	    private Button btnXacNhan;
+
+	    @FXML
+	    private Button btnXoa;
+
+	    @FXML
+	    private DatePicker dpNgayHetHan;
+
+	    @FXML
+	    private TextField txtDonVi;
+
+	    @FXML
+	    private TextField txtLoaiThucPham;
+
+	    @FXML
+	    private TextField txtSoLuong;
+
+	    @FXML
+	    private TextField txtTenThucPham;
+	    
+	    private FridgeEntity food;
+
+	private int type;
+
+	private FridgeFoodController controller;
+
+	private UserEntity userLogin;
+	
+	public void close(ActionEvent event) {
+		Node node = (Node) event.getSource();
+		Stage formAddFood = (Stage) node.getScene().getWindow();
+
+		formAddFood.close();
+	}
+
+	@FXML
+	void delete(ActionEvent event) {
+		
+		int userId = userLogin.getUser_id();
+
+		boolean deleteOk = controller.deleteFood(food.getFood_id(), userId);
+
+		if (deleteOk == false) {
+			Alert a = new Alert(AlertType.WARNING, "Thực phẩm không có!", ButtonType.OK);
+			a.setHeaderText(null);
+			a.showAndWait();
+			return;
+		}
+
+		Alert a = new Alert(AlertType.INFORMATION, "Xóa thực phẩm thành công!", ButtonType.OK);
+		a.setHeaderText(null);
+		a.showAndWait();
+		close(event);
+	}
+
+	@FXML
+	void selectFoodNeedAdd(ActionEvent event) {
+		TextInputDialog addFood = new TextInputDialog();
+
+		addFood.setHeaderText("Nhập vào thực phẩm cần thêm!");
+		addFood.showAndWait();
+
+		String nameFood = addFood.getEditor().getText();
+
+		food = controller.getFoodModelByName(nameFood);
+
+		if (food == null) {
+			Alert a = new Alert(AlertType.WARNING, "Thực phẩm không có!", ButtonType.OK);
+			a.setHeaderText(null);
+			a.showAndWait();
+			return;
+		}
+
+		txtTenThucPham.setText(food.getRaw_food_name());
+		txtLoaiThucPham.setText(food.getFood_typeString());
+		txtDonVi.setText(food.getUnit());
+	}
+
+	@FXML
+	void submit(ActionEvent event) {
+		if (type == 1) {
+			if (txtTenThucPham.getText().isEmpty()) {
+				Alert a = new Alert(AlertType.WARNING, "Hãy chọn thực phẩm cần thêm!", ButtonType.OK);
+				a.setHeaderText(null);
+				a.showAndWait();
+				return;
+			}
+
+			if (dpNgayHetHan.getValue() == null) {
+				Alert a = new Alert(AlertType.WARNING, "Vui lòng nhập vào ngày hết hạn!", ButtonType.OK);
+				a.setHeaderText(null);
+				a.showAndWait();
+				return;
+			}
+			
+			if (txtSoLuong.getText().isEmpty()) {
+				Alert a = new Alert(AlertType.WARNING, "Vui lòng nhập vào số lượng!", ButtonType.OK);
+				a.setHeaderText(null);
+				a.showAndWait();
+				return;
+			}
+
+			Boolean insertOk = controller.insertFoodIntoFridge(food.getRaw_food_name(), 
+															   userLogin.getUser_id(), 
+															   Integer.parseInt(txtSoLuong.getText()), 
+															   dpNgayHetHan.getValue().toString());
+
+			if (insertOk == false) {
+				Alert a = new Alert(AlertType.WARNING, "Thực phẩm đã có!", ButtonType.OK);
+				a.setHeaderText(null);
+				a.showAndWait();
+
+				txtTenThucPham.setText("");
+				txtLoaiThucPham.setText("");
+				txtDonVi.setText("");
+
+				return;
+			}
+			
+		} else {
+			controller.updateFood(food.getFood_id(), userLogin.getUser_id(),
+					Integer.parseInt(txtSoLuong.getText()));
+		}
+
+		close(event);
+	}
+
+	public void setFoodData(FridgeEntity foodCurrent) {
+		this.food = foodCurrent;
+		txtTenThucPham.setText(food.getRaw_food_name());
+		txtLoaiThucPham.setText(food.getFood_typeString());
+		dpNgayHetHan.setValue(LocalDate.parse(food.getExpiry_date()));
+		txtDonVi.setText(food.getUnit());
+		txtSoLuong.setText(String.valueOf(food.getNumber()));
+	}
+
+	public void setChangeOrAdd(int type) {
+		this.type = type;
+
+		if (type == 1) {
+			btnXoa.setDisable(true);
+		} else {
+			dpNgayHetHan.setDisable(true);
+		}
+	}
+
+	@FXML
+	private void initialize() {
+		controller = new FridgeFoodController();
+
+		userLogin = UserSingleton.getInstance();
+		
+	}
+}
